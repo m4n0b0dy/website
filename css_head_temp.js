76dd4097d89483c903908c77fbc951d19fe0be34
kvenuti <kvenuti@gmail.com>
2017-03-19 18:43:22 -0400

    Update css_head_temp.js

--- css_head_temp.js
+++ css_head_temp.js
@@ -2,7 +2,7 @@ document.write('\
 \
 <head>\
 	<meta charset="UTF-8">\
-	<link rel="icon" href="pictures/logo.jpg">\
+	<link rel="icon" href="pictures/logo.JPG">\
 	<link rel="stylesheet" href="css/base_style.css">\
 	<link rel="stylesheet" href="css/fonts.css">\
 </head>\

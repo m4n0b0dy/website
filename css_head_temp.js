92c608b45c6e5ddc12e1da7d5120d0cdecfe19c4
kvenuti <kvenuti@gmail.com>
2017-03-20 10:41:05 -0400

    Add files via upload

---
+++ css_head_temp.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<head>\
+	<meta charset="UTF-8">\
+	<link rel="icon" href="pictures/logo.JPG">\
+	<link rel="stylesheet" href="css/base_style.css">\
+	<link rel="stylesheet" href="css/fonts.css">\
+	<script>\
+  	(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+  	ga("create", "UA-94046520-1", "auto");
+  	ga("send", "pageview");
+	</script>\
+</head>\
+\
+');

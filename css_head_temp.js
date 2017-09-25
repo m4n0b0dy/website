9c75372d5be32db9a53803655af03fc2992fb80e
kvenuti <kvenuti@gmail.com>
2017-09-25 13:00:15 -0700

    quik fix

--- css_head_temp.js
+++ css_head_temp.js
@@ -5,7 +5,6 @@ document.write('\
 	<link rel="icon" href="pictures/logo.JPG">\
 	<link rel="stylesheet" href="css/base_style.css">\
 	<link rel="stylesheet" href="css/fonts.css">\
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107043850-1"></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', 'UA-107043850-1');</script>\
 </head>\
 \
 ');

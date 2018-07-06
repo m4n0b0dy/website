82c9e8137fbd4e021e6e62fa1dac1c2498d35e86
Keenan Venuti <kvenuti@gmail.com>
2018-07-06 11:04:09 -0700

    made multi month for sotd

---
+++ past_song_months/css_head_temp_pm.js
@@ -0,0 +1,10 @@
+document.write('\
+\
+<head>\
+	<meta charset="UTF-8">\
+	<link rel="icon" href="../pictures/logo.JPG">\
+	<link rel="stylesheet" href="../css/base_style.css">\
+	<link rel="stylesheet" href="../css/fonts.css">\
+</head>\
+\
+');
---
+++ past_song_months/header_temp_pm.js
@@ -0,0 +1,20 @@
+document.write('\
+\
+<body>\
+  <div id="topBar">\
+    <a id="initials" href="../home.html" target ="_blank">KV</a>\
+    <a id="top_pic" href="https://www.linkedin.com/in/keenan-venuti-224a288a" target ="_blank">\
+    <img src="../pictures/me.jpg" style="width: 80px;"></img></a>\
+  </div>\
+\
+  <div id="centerColumn">\
+    <div id="navwrap_but">\
+      <nav>\
+  <li><a href="../home.html">Home</a></li>\
+  <li><a href="../projects.html">Projects</a></li>\
+  <li><a href="../blogs.html">Blogs</a></li>\
+  <li><a href="../contact.html">Contact</a></li>\
+      </nav>\
+    </div>\
+\
+');

d8dbacf9dd7ae29da709ae94cd9e280cf71f7152
Nobody <kvenuti@gmail.com>
2019-11-10 17:00:01 -0800

    changed sotd to song of the week and updated home image

---
+++ past_song_years/header_temp.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<body>\
+  <div id="topBar">\
+    <a id="initials" href="https://www.linkedin.com/in/keenan-venuti-224a288a" target ="_blank">KV</a>\
+  </div>\
+\
+  <div id="centerColumn">\
+    <div id="navwrap_but">\
+      <nav>\
+  <li><a href="../home.html">Home</a></li>\
+  <li><a href="../projects.html">Projects</a></li>\
+  <li><a href="../blogs.html">Blogs</a></li>\
+  <li><a href="../contact.html">Contact</a></li>\
+      </nav>\
+    </div>\
+\
+');
